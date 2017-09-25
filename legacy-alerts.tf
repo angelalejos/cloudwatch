5924aa87fd50d85e21aed5f059ba7422f0708694
Julian Simioni <julian@mapzen.com>
2017-09-25 12:26:06 -0400

    Add healthy host counts alert for legacy (non-k8s) ELBs

---
+++ legacy-alerts.tf
@@ -0,0 +1,167 @@
+# Legacy alerts for Elastic Load Balancers of non-Kubernetes Pelias infrastructure
+
+# the common theme is to have one non-critical alert for any unhealthy hosts, and one critical for
+# zero healthy hosts, for each ELB
+
+resource "aws_cloudwatch_metric_alarm" "api-elb-unhealthy-host-alert" {
+  alarm_name                = "${var.service_name}-${var.environment}-api-elb-unhealthy-host-alert"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "3"
+  metric_name               = "UnHealthyHostCount"
+  namespace                 = "AWS/ELB"
+  period                    = "60"
+  statistic                 = "Maximum"
+  threshold                 = "1"
+  alarm_description         = "This metric monitors the legacy (non-kubernetes) Pelias API ELB for unhealthy hosts. Even one unhealthy host sends a non-critical alert"
+  insufficient_data_actions = []
+
+  actions_enabled = "${var.legacy_alerts_enabled ? 1 : 0}"
+  alarm_actions   = ["${data.aws_sns_topic.non-critical-alerts.arn}"]
+
+  dimensions {
+    LoadBalancerName = "${var.legacy-pelias-api-elb-name}"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "api-elb-zero-healthy-host-alert" {
+  alarm_name                = "${var.service_name}-${var.environment}-api-elb-healthy-host-alert"
+  comparison_operator       = "LessThanOrEqualToThreshold"
+  evaluation_periods        = "3"
+  metric_name               = "HealthyHostCount"
+  namespace                 = "AWS/ELB"
+  period                    = "60"
+  statistic                 = "Minimum"
+  threshold                 = "0"
+  alarm_description         = "This metric monitors the legacy (non-kubernetes) Pelias API ELB for healthy hosts. It sends a critical alert if there are no healthy hosts"
+  insufficient_data_actions = []
+
+  actions_enabled = "${var.legacy_alerts_enabled ? 1 : 0}"
+  alarm_actions   = ["${data.aws_sns_topic.critical-alerts.arn}"]
+
+  dimensions {
+    LoadBalancerName = "${var.legacy-pelias-api-elb-name}"
+  }
+}
+
+# PIP
+resource "aws_cloudwatch_metric_alarm" "pip-elb-unhealthy-host-alert" {
+  alarm_name                = "${var.service_name}-${var.environment}-pip-elb-unhealthy-host-alert"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "3"
+  metric_name               = "UnHealthyHostCount"
+  namespace                 = "AWS/ELB"
+  period                    = "60"
+  statistic                 = "Maximum"
+  threshold                 = "1"
+  alarm_description         = "This metric monitors the legacy (non-kubernetes) Pelias PIP ELB for unhealthy hosts. Even one unhealthy host sends a non-critical alert"
+  insufficient_data_actions = []
+
+  actions_enabled = "${var.legacy_alerts_enabled ? 1 : 0}"
+  alarm_actions   = ["${data.aws_sns_topic.non-critical-alerts.arn}"]
+
+  dimensions {
+    LoadBalancerName = "${var.legacy-pelias-pip-elb-name}"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "pip-elb-zero-healthy-host-alert" {
+  alarm_name                = "${var.service_name}-${var.environment}-pip-elb-healthy-host-alert"
+  comparison_operator       = "LessThanOrEqualToThreshold"
+  evaluation_periods        = "3"
+  metric_name               = "HealthyHostCount"
+  namespace                 = "AWS/ELB"
+  period                    = "60"
+  statistic                 = "Minimum"
+  threshold                 = "0"
+  alarm_description         = "This metric monitors the legacy (non-kubernetes) Pelias PIP ELB for healthy hosts. It sends a critical alert if there are no healthy hosts"
+  insufficient_data_actions = []
+
+  actions_enabled = "${var.legacy_alerts_enabled ? 1 : 0}"
+  alarm_actions   = ["${data.aws_sns_topic.critical-alerts.arn}"]
+
+  dimensions {
+    LoadBalancerName = "${var.legacy-pelias-pip-elb-name}"
+  }
+}
+
+## Placeholder
+resource "aws_cloudwatch_metric_alarm" "placeholder-elb-unhealthy-host-alert" {
+  alarm_name                = "${var.service_name}-${var.environment}-placeholder-elb-unhealthy-host-alert"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "3"
+  metric_name               = "UnHealthyHostCount"
+  namespace                 = "AWS/ELB"
+  period                    = "60"
+  statistic                 = "Maximum"
+  threshold                 = "1"
+  alarm_description         = "This metric monitors the legacy (non-kubernetes) Pelias Placeholder ELB for unhealthy hosts. Even one unhealthy host sends a non-critical alert"
+  insufficient_data_actions = []
+
+  actions_enabled = "${var.legacy_alerts_enabled ? 1 : 0}"
+  alarm_actions   = ["${data.aws_sns_topic.non-critical-alerts.arn}"]
+
+  dimensions {
+    LoadBalancerName = "${var.legacy-pelias-placeholder-elb-name}"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "placeholder-elb-zero-healthy-host-alert" {
+  alarm_name                = "${var.service_name}-${var.environment}-placeholder-elb-healthy-host-alert"
+  comparison_operator       = "LessThanOrEqualToThreshold"
+  evaluation_periods        = "3"
+  metric_name               = "HealthyHostCount"
+  namespace                 = "AWS/ELB"
+  period                    = "60"
+  statistic                 = "Minimum"
+  threshold                 = "0"
+  alarm_description         = "This metric monitors the legacy (non-kubernetes) Pelias Placeholder ELB for healthy hosts. It sends a critical alert if there are no healthy hosts"
+  insufficient_data_actions = []
+
+  actions_enabled = "${var.legacy_alerts_enabled ? 1 : 0}"
+  alarm_actions   = ["${data.aws_sns_topic.critical-alerts.arn}"]
+
+  dimensions {
+    LoadBalancerName = "${var.legacy-pelias-placeholder-elb-name}"
+  }
+}
+
+## Interpolation
+resource "aws_cloudwatch_metric_alarm" "interpolation-elb-unhealthy-host-alert" {
+  alarm_name                = "${var.service_name}-${var.environment}-interpolation-elb-unhealthy-host-alert"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "3"
+  metric_name               = "UnHealthyHostCount"
+  namespace                 = "AWS/ELB"
+  period                    = "60"
+  statistic                 = "Maximum"
+  threshold                 = "1"
+  alarm_description         = "This metric monitors the legacy (non-kubernetes) Pelias interpolation ELB for unhealthy hosts. Even one unhealthy host sends a non-critical alert"
+  insufficient_data_actions = []
+
+  actions_enabled = "${var.legacy_alerts_enabled ? 1 : 0}"
+  alarm_actions   = ["${data.aws_sns_topic.non-critical-alerts.arn}"]
+
+  dimensions {
+    LoadBalancerName = "${var.legacy-pelias-interpolation-elb-name}"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "interpolation-elb-zero-healthy-host-alert" {
+  alarm_name                = "${var.service_name}-${var.environment}-interpolation-elb-healthy-host-alert"
+  comparison_operator       = "LessThanOrEqualToThreshold"
+  evaluation_periods        = "3"
+  metric_name               = "HealthyHostCount"
+  namespace                 = "AWS/ELB"
+  period                    = "60"
+  statistic                 = "Minimum"
+  threshold                 = "0"
+  alarm_description         = "This metric monitors the legacy (non-kubernetes) Pelias Interpolation ELB for healthy hosts. It sends a critical alert if there are no healthy hosts"
+  insufficient_data_actions = []
+
+  actions_enabled = "${var.legacy_alerts_enabled ? 1 : 0}"
+  alarm_actions   = ["${data.aws_sns_topic.critical-alerts.arn}"]
+
+  dimensions {
+    LoadBalancerName = "${var.legacy-pelias-interpolation-elb-name}"
+  }
+}
--- variables.tf
+++ variables.tf
@@ -29,3 +29,27 @@ variable "environment" {
   description = "Which environment (dev, staging, prod, etc) this group of machines is for"
   default     = "dev"
 }
+
+variable "legacy-pelias-api-elb-name" {
+  description = "The full name of the ELB running the legacy Pelias API (not in kubernetes). If blank no alerts for this ELB will be created"
+}
+
+variable "legacy-pelias-placeholder-elb-name" {}
+variable "legacy-pelias-pip-elb-name" {}
+variable "legacy-pelias-interpolation-elb-name" {}
+
+variable "critical-alert-sns-topic-name" {
+  description = "An Amazon SNS topic (for example, a PagerDuty event endpoint) that will be used as the alarm action for CRITICAL alerts"
+}
+
+variable "non-critical-alert-sns-topic-name" {
+  description = "An Amazon SNS topic (for example, a PagerDuty event endpoint) that will be used as the alarm action for non-critical alerts"
+}
+
+variable "alerts_enabled" {
+  default = "true"
+}
+
+variable "legacy_alerts_enabled" {
+  default = "true"
+}
